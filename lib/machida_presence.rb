60e607246e30100d42eb8882772fd74d9a6baef1
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-07-03 20:38:38 +0900

    bundle gem machida_presence

---
+++ lib/machida_presence.rb
@@ -0,0 +1,6 @@
+require "machida_presence/version"
+
+module MachidaPresence
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/machida_presence/version.rb
@@ -0,0 +1,3 @@
+module MachidaPresence
+  VERSION = "0.1.0"
+end
